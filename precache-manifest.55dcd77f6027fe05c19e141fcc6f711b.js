cfbbfbafc24b2a852bc3ba188e705c310f2864c1
Pardeg <pardeg@gmail.com>
2020-05-17 00:01:19 +0300

    Updates

---
+++ precache-manifest.55dcd77f6027fe05c19e141fcc6f711b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07d85e437b92dba826c407fada22afd",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "4ae38c12284a4312aba5",
+    "url": "/aviasales/static/js/2.a9acd4f9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/aviasales/static/js/2.a9acd4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e7e3842506a2759195",
+    "url": "/aviasales/static/js/main.60f20d47.chunk.js"
+  },
+  {
+    "revision": "d966f2b871a96fd638ae",
+    "url": "/aviasales/static/js/runtime-main.7eab63ba.js"
+  },
+  {
+    "revision": "fc4db7ee86a2b160e9520aee39d5338f",
+    "url": "/aviasales/static/media/Checkbox.fc4db7ee.svg"
+  },
+  {
+    "revision": "74238712fd7d703c9635fe2b67d6b1e9",
+    "url": "/aviasales/static/media/Form.74238712.svg"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales/static/media/mainLogo.0c089234.svg"
+  }
+]);
